3bb9d347184a9100e3c24faa7a893b2d788f56d6
Rodrigo <dulo.rodrigo@gmail.com>
2019-03-28 17:50:30 -0600

    Genera y guarda usuario

--- sockets/sockets.ts
+++ sockets/sockets.ts
@@ -1,15 +1,22 @@
 // Lógica de los sockets
 import { Socket } from "socket.io";
+import { UsuariosLista } from "../clases/usuarios-lista";
+import { Usuario } from "../clases/usuario";
 
 interface Mensaje {
   de: string;
   cuerpo: string;
 }
 
+export const usuariosConectados = new UsuariosLista();
+
 // Desconectar socket
 export const desconectar = ( cliente: Socket ) => {
   cliente.on('disconnect', () => {
     console.log('Cliente desconectado');
+    usuariosConectados.borrarUsuario(cliente.id);
+    console.log( usuariosConectados.getLista() );
+    
   });
 }
 // Escuchar mensajes
@@ -18,4 +25,24 @@ export const mensaje = ( cliente: Socket, io: SocketIO.Server ) => {
     console.log('Mensaje recibido:', payload);
     io.emit('mensaje-nuevo', payload);
   });
+}
+
+// Configurar usuario
+export const configurarUsuario = ( cliente: Socket, io: SocketIO.Server ) => {
+  cliente.on('configurar-usuario', ( payload: { nombre: string }, callback: Function ) => {
+
+    usuariosConectados.actualizarNombre(cliente.id, payload.nombre);
+
+    callback({
+      ok: true,
+      mensaje: `Usuario ${ payload.nombre } configurado.`
+    });
+  });
+}
+
+// Conectar cliente
+export const conectarCliente = ( cliente: Socket ) => {
+  const usuario = new Usuario( cliente.id );
+  usuariosConectados.agregar(usuario);
+  console.log(usuario);
 }
