eb2338500ea9cd827311269f54a2d10805fe7269
Wako <dulo.rodrigo@gmail.com>
2019-10-08 11:07:43 -0500

    Envio de mensaje desde postman

--- backend/routes/router.ts
+++ backend/routes/router.ts
@@ -1,4 +1,5 @@
 import { Router, Request, Response} from 'express';
+import Server from '../classes/server';
 
 const router = Router();
 
@@ -11,20 +12,38 @@ router.get('/mensajes', (req: Request, res: Response) => {
 
 router.post('/mensajes', (req: Request, res: Response) => {
   const { body } = req;
+  const { cuerpo, de } = body;
+  const payload = {
+    cuerpo,
+    de
+  }
+  const server = Server.instance;
+  server.io.emit('mensaje-nuevo', payload);
   res.json({
     ok: true,
-    msj: 'post',
-    body
+    cuerpo,
+    de
   })
 });
 
 router.post('/mensajes/:id', (req: Request, res: Response) => {
   const { body, params } = req;
+  const { cuerpo, de } = body;
+  const { id } = params;
+
+  const payload = {
+    de,
+    cuerpo
+  }
+
+  const server = Server.instance;
+  server.io.in(id).emit('mensaje-privado', payload);
+
   res.json({
     ok: true,
-    msj: 'post',
-    body,
-    params
+    cuerpo,
+    de,
+    id
   })
 });
 
