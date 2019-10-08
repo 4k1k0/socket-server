68c9a531ad791e629a1c84ba3c816384dd1783f4
Wako <dulo.rodrigo@gmail.com>
2019-10-08 16:13:56 -0500

    Instala paquetes

--- grafica/frontend/src/app/app.module.ts
+++ grafica/frontend/src/app/app.module.ts
@@ -1,6 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { environment } from '../environments/environment';
+
+// Sockets
+import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
+
+const config: SocketIoConfig = { url: environment.wsUrl, options: {} };
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -10,7 +17,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SocketIoModule.forRoot(config)
   ],
   providers: [],
   bootstrap: [AppComponent]
